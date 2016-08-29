6be9e35e9fd746d79eec825600d0de25ba5aff6c
Adam Luchini <aluchini@gmail.com>
2016-08-29 16:08:53 -0700

    direction attempt

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   map: Ember.inject.service('google-map'),
-
+  first: [45.522462, -122.665674],
+  second: [46.522462, -122.665674],
+  third: [47.522462, -122.665674],
+  fourth: [48.522462, -122.665674],
   actions: {
     showMap(model) {
       console.log(model);
@@ -13,7 +16,6 @@ export default Ember.Component.extend({
       };
       var fullMap = this.get('map').findMap(container, options);
       var map = this.get('map');
-
       model.forEach(function(location){
         console.log(location.get('name'));
         var latLng = {lat: location.get('latitude'), lng: location.get('longitude')};
@@ -24,6 +26,35 @@ export default Ember.Component.extend({
           title: name
         });
         marker.setMap(fullMap);
+      });
+    },
+    showRouteMap() {
+      var container = this.$('.map-display')[0];
+      var directionsService = this.get('map').directionsService();
+      var directionsDisplay = this.get('map').directionsDisplay();
+      var options = {
+        center: this.get('map').center(45.522462, -122.665674),
+        zoom: 15
+      };
+      var fullMap = this.get('map').findMap(container, options);
+      directionsDisplay.setMap(fullMap);
+      var request = {
+        origin: this.get('first'),
+        destination: this.get('fourth'),
+        travelMode: 'BICYCLING',
+        waypoints: [
+        {
+          location: this.get('second'),
+          stopover: true
+        },{
+          location: this.get('third'),
+          stopover: true
+        }]
+      };
+      directionsService.route(request, function(result, status) {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(result);
+        }
       });
     }
   }
--- app/services/google-map.js
+++ app/services/google-map.js
@@ -7,5 +7,46 @@ export default Ember.Service.extend({
   },
   center(latitude, longitude) {
     return new this.googleMaps.LatLng(latitude, longitude);
+  },
+  directionsService() {
+    return new google.maps.DirectionsService();
+  },
+  directionsDisplay() {
+    return new google.maps.DirectionsRenderer();
+  }
+
+  waypointRouting(first, second, third, fourth) {
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
+    var map;
+    function initialize() {
+      directionsDisplay = new google.maps.DirectionsRenderer();
+      var portland = new google.maps.LatLng(44.850033, -122.6500523);
+      var mapOptions = {
+        zoom:7,
+        center: portland
+      }
+      map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      directionsDisplay.setMap(map);
+    }
+    function calcRoute() {
+      var request = {
+        origin: first,
+        destination: fourth,
+        travelMode: 'BICYCLING',
+        waypoints[]: [{
+          location: second,
+          stopover: true
+        },{
+          location: third,
+          stopover: true
+        }]
+      };
+      directionsService.route(request, function(result, status) {
+        if (status == 'OK') {
+          directionsDisplay.setDirections(result);
+        }
+      });
+    }
   }
 });
