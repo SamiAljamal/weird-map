f60b44331dc777333d7aefba2ec526ff63b39c80
Adam Luchini and George Olson <george@>
2016-08-29 10:42:43 -0700

    Add marker to map of location on individual location pages

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -5,12 +5,21 @@ export default Ember.Component.extend({
 
   actions: {
     showMap(location) {
+      console.log(location);
       var container = this.$('.map-display')[0];
       var options = {
         center: this.get('map').center(location.get('latitude'), location.get('longitude')),
         zoom: 15
       };
-      this.get('map').findMap(container, options);
+      var fullMap = this.get('map').findMap(container, options);
+      var latLng = {lat: location.get('latitude'), lng: location.get('longitude')};
+      var map = this.get('map');
+      var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        title: 'Hello World!'
+      });
+      marker.setMap(fullMap);
     }
   }
 });
